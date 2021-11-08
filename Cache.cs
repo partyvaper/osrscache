f6cf99c9a5301192bed2ac916a2212bac41ccc0f
Davis Miculis <58113606+partyvaper@users.noreply.github.com>
2021-11-08 20:04:34 +0200

    updates

--- Cache.cs
+++ Cache.cs
@@ -12,112 +12,92 @@ using CommandLine;
 
 namespace OSRSCache {
 
+	public enum IndexType: int
+	{
+		FRAMES = 0,
+		FRAMEMAPS = 1,
+		CONFIGS = 2,
+		INTERFACES = 3,
+		SOUNDEFFECTS = 4,
+		MAPS = 5,
+		TRACK1 = 6,
+		MODELS = 7,
+		SPRITES = 8,
+		TEXTURES = 9,
+		BINARY = 10,
+		TRACK2 = 11,
+		CLIENTSCRIPT = 12,
+		FONTS = 13,
+		VORBIS = 14,
+		INSTRUMENTS = 15,
+		WORLDMAP = 16,
+	}
+
 	internal class Cache
 	{
 		public class Options
 		{
-			[Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+			[Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages")]
 			public bool Verbose { get; set; }
+			[Option('c', "cache", Required = false, HelpText = "Set the cache folder directory")]
+			public string Cache { get; set; }
+			[Option('i', "items", Required = false, HelpText = "Dump items config to folder")]
+			public bool Items { get; set; }
+			[Option('n', "npcs", Required = false, HelpText = "Dump npcs config to folder")]
+			public bool Npcs { get; set; }
+			[Option('o', "objects", Required = false, HelpText = "Dump objects config to folder")]
+			public bool Objects { get; set; }
+			[Option('s', "sprites", Required = false, HelpText = "Dump sprites images to folder")]
+			public bool Sprites { get; set; }
 		}
 		
 		public static void Main(string[] args) // throws IOException
 		{
-			Parser.Default.ParseArguments<Options>(args)
-				.WithParsed<Options>(o => {
-					if (o.Verbose)
-					{
-						Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-						Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-					}
-					else
-					{
-						Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-						Console.WriteLine("Quick Start Example!");
-					}
-				});
-			
-			// Options options = new Options();
-			//
-			// options.addOption("c", "cache", true, "cache base");
-			//
-			// options.addOption(null, "items", true, "directory to dump items to");
-			// options.addOption(null, "npcs", true, "directory to dump npcs to");
-			// options.addOption(null, "objects", true, "directory to dump objects to");
-			// options.addOption(null, "sprites", true, "directory to dump sprites to");
-			//
-			// CommandLineParser parser = new DefaultParser();
-			// CommandLine cmd;
-			// try
-			// {
-			// 	cmd = parser.parse(options, args);
-			// }
-			// catch (ParseException ex)
-			// {
-			// 	Console.WriteLine("Error parsing command line options: " + ex.getMessage());
-			// 	System.exit(-1);
-			// 	return;
-			// }
-			//
-			// string cache = cmd.getOptionValue("cache");
-			//
-			// Store store = loadStore(cache);
-			//
-			// if (cmd.hasOption("items"))
-			// {
-			// 	string itemdir = cmd.getOptionValue("items");
-			//
-			// 	if (itemdir == null)
-			// 	{
-			// 		Console.WriteLine("Item directory must be specified");
-			// 		return;
-			// 	}
-			//
-			// 	Console.WriteLine("Dumping items to " + itemdir);
-			// 	dumpItems(store, new File(itemdir));
-			// }
-			// else if (cmd.hasOption("npcs"))
-			// {
-			// 	string npcdir = cmd.getOptionValue("npcs");
-			//
-			// 	if (npcdir == null)
-			// 	{
-			// 		Console.WriteLine("NPC directory must be specified");
-			// 		return;
-			// 	}
-			//
-			// 	Console.WriteLine("Dumping npcs to " + npcdir);
-			// 	dumpNpcs(store, new File(npcdir));
-			// }
-			// else if (cmd.hasOption("objects"))
-			// {
-			// 	string objectdir = cmd.getOptionValue("objects");
-			//
-			// 	if (objectdir == null)
-			// 	{
-			// 		Console.WriteLine("Object directory must be specified");
-			// 		return;
-			// 	}
-			//
-			// 	Console.WriteLine("Dumping objects to " + objectdir);
-			// 	dumpObjects(store, new File(objectdir));
-			// }
-			// else if (cmd.hasOption("sprites"))
-			// {
-			// 	string spritedir = cmd.getOptionValue("sprites");
-			//
-			// 	if (spritedir == null)
-			// 	{
-			// 		Console.WriteLine("Sprite directory must be specified");
-			// 		return;
-			// 	}
-			//
-			// 	Console.WriteLine("Dumping sprites to " + spritedir);
-			// 	dumpSprites(store, new File(spritedir));
-			// }
-			// else
-			// {
-			// 	Console.WriteLine("Nothing to do");
-			// }
+			Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => {
+				Console.WriteLine($"Verbose set to: {o.Verbose}");
+
+				string cacheDir = o.Cache;
+				if (cacheDir == null) {
+					cacheDir = "cache";
+				}
+
+				string outDir = "dumps";
+
+				// TODO: flatcache
+				
+				Store store = loadStore(cacheDir);
+				
+				if (o.Items)
+				{
+					string dir = outDir + "/items";
+					Console.WriteLine("Dumping items to " + dir);
+					dumpItems(store, new File(dir));
+				}
+				else if (o.Npcs)
+				{
+					string dir = outDir + "/npcs";
+					Console.WriteLine("Dumping npcs to " + dir);
+					dumpNpcs(store, new File(dir));
+				}
+				else if (o.Objects)
+				{
+					string dir = outDir + "/objects";
+					Console.WriteLine("Dumping objects to " + dir);
+					dumpObjects(store, new File(dir));
+				}
+				else if (o.Sprites)
+				{
+					string dir = outDir + "/sprites";
+					Console.WriteLine("Dumping sprites to " + dir);
+					dumpSprites(store, new File(dir));
+				}
+				else
+				{
+					Console.WriteLine("Nothing to do");
+				}
+				
+				System.Environment.Exit(1);
+			});
 		}
 
 		private static Store loadStore(string cache) // throws IOException
